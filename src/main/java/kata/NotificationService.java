a053e62d37e53d02918d6af15026ddfc88764307
Aaron Uurman <uurman@rushstreetinteractive.com>
2022-04-25 10:45:34 +0300

    feat: add support for multiple messages in upcoming delivery notification feature

--- src/main/java/kata/NotificationService.java
+++ src/main/java/kata/NotificationService.java
@@ -6,11 +6,12 @@ import java.time.format.DateTimeFormatter;
 public class NotificationService {
 
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    public static final String UPCOMING_DELIVERY_MESSAGE_TEMPLATE_FOR_SMS = "Your delivery arrives in %s minutes.";
 
     private final SendgridEmailGateway emailGateway;
     private final SmsGateway smsGateway;
 
-    private static final String UPCOMING_DELIVERY_MESSAGE_TEMPLATE = "Your delivery to [%s,%s] is next, estimated time of arrival is in %s minutes. Be ready!";
+    private static final String UPCOMING_DELIVERY_MESSAGE_TEMPLATE_FOR_EMAIL = "Your delivery to [%s,%s] is next, estimated time of arrival is in %s minutes. Be ready!";
     private static final String RECOMMENDATION_MESSAGE_TEMPLATE_FOR_EMAIL = "Regarding your delivery today at %s. How likely would you be to recommend this delivery service to a friend? Click <a href='url'>here</a>";
     private static final String RECOMMENDATION_MESSAGE_TEMPLATE_FOR_SMS = "How likely would you be to recommend this delivery service to a friend? Click <a href='url'>here</a>";
 
@@ -25,15 +26,14 @@ public class NotificationService {
 
     public void upcomingDelivery(Delivery delivery, Duration eta) {
         var subject = "Your delivery will arrive soon";
-        var message = UPCOMING_DELIVERY_MESSAGE_TEMPLATE.formatted(
+        var message = UPCOMING_DELIVERY_MESSAGE_TEMPLATE_FOR_EMAIL.formatted(
                 delivery.getCoordinates().latitude(),
                 delivery.getCoordinates().longitude(),
                 eta.toMinutes()
         );
         if (delivery.hasPhoneNumber()) {
-            smsGateway.send(delivery.getPhoneNumber(), message);
-        }
-        else {
+            smsGateway.send(delivery.getPhoneNumber(), UPCOMING_DELIVERY_MESSAGE_TEMPLATE_FOR_SMS.formatted(eta.toMinutes()));
+        } else {
             emailGateway.send(delivery.getContactEmail(), subject, message);
         }
     }
